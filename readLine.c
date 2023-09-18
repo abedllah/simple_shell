cfa28692b9406afc9b0e12d753b8326d11f3c328
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-18 02:38:39 +0200

    .

--- readLine.c
+++ readLine.c
@@ -2,15 +2,25 @@
 
 char *read_line(void) 
 {
-    char *line;
+    char *line = NULL;
     size_t len = 0;  
 
     if (isatty(STDIN_FILENO))
-        write(STDOUT_FILENO, "$ ",2);
-    if (getline(&line, &len, stdin) == -1) 
+        write(STDOUT_FILENO, "$ ", 2);
+    
+    ssize_t read_result = getline(&line, &len, stdin);
+    
+    if (read_result == -1) 
     {
         free(line);
         return NULL;
     }
+
+    // Remove the newline character at the end, if present
+    if (line[read_result - 1] == '\n') 
+    {
+        line[read_result - 1] = '\0';
+    }
+
     return line;
 }
