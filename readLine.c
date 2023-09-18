ff49be246969377da72fc5a4f589e54502eab806
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-18 02:42:55 +0200

    .

--- readLine.c
+++ readLine.c
@@ -7,16 +7,17 @@ char *read_line(void)
 
     if (isatty(STDIN_FILENO))
         write(STDOUT_FILENO, "$ ", 2);
-    
-    ssize_t read_result = getline(&line, &len, stdin);
-    
+
+    ssize_t read_result;
+    read_result = getline(&line, &len, stdin);
+
     if (read_result == -1) 
     {
         free(line);
         return NULL;
     }
 
-    // Remove the newline character at the end, if present
+    /* Remove the newline character at the end, if present */
     if (line[read_result - 1] == '\n') 
     {
         line[read_result - 1] = '\0';
