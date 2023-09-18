d3c44df19c1f4e080202dd4a4afcfeb1d4131ec0
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-18 02:27:58 +0200

    .

--- readLine.c
+++ readLine.c
@@ -2,44 +2,15 @@
 
 char *read_line(void) 
 {
-    char *line = NULL;
+    char *line;
     size_t len = 0;  
-    ssize_t read_result;
 
     if (isatty(STDIN_FILENO))
         write(STDOUT_FILENO, "$ ",2);
-
-    char buffer[1024];
-    
-    if ((read_result = read(STDIN_FILENO, buffer, sizeof(buffer))) == -1) 
+    if (getline(&line, &len, stdin) == -1) 
     {
-        perror("read");
+        free(line);
         return NULL;
     }
-
-    if (read_result == 0) 
-    {
-        return NULL; 
-    }
-
-    if (buffer[read_result - 1] == '\n') 
-    {
-        buffer[read_result - 1] = '\0';
-        len = read_result;
-    } 
-    else 
-    {
-        len = read_result + 1;
-        buffer[read_result] = '\0';
-    }
-
-    line = malloc(len);
-    if (line == NULL) 
-    {
-        perror("malloc");
-        return NULL;
-    }
-    strcpy(line, buffer);
-
     return line;
 }
