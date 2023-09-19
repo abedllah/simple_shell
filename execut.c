ebc732397d4830c9794cee2aa8e8787363b85edf
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-20 00:06:11 +0200

    handel the path

--- execut.c
+++ execut.c
@@ -1,6 +1,6 @@
 #include "simple_shell.h"
 
-int execute(char **args, char **argv) 
+/* int execute(char **args, char **argv) 
 {
     pid_t child;
     int action;
@@ -20,6 +20,44 @@ int execute(char **args, char **argv)
         freeArr(args);
     }
     return WEXITSTATUS(action);
+} */
+
+int execute(char **args, char **argv) 
+{
+    pid_t child;
+    int action;
+
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path);
+    char *token = strtok(path_copy, ":");
+    while (token != NULL)
+    {
+        char command_path[1024];  
+        snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
+        
+        if (access(command_path, X_OK) == 0) 
+        {
+            child = fork();
+            if (child == 0)
+            {
+                if (execve(command_path, args, envir) == -1)
+                {
+                    perror(argv[0]); 
+                    _exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                waitpid(child, &action, 0);
+                freeArr(args);
+            }
+            free(path_copy);
+            return WEXITSTATUS(action);
+        }
+        token = strtok(NULL, ":");
+    }
+
+    fprintf(stderr, "%s: command not found\n", args[0]);
+    free(path_copy);
+    return -1;
 }
-
-
