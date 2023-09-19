f982aabeb2ed9988d983f7262726e7c61fe3c7b6
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-20 00:20:41 +0200

    execute a command with the full path and short path

--- execut.c
+++ execut.c
@@ -5,7 +5,7 @@ int execute(char **args, char **argv)
     pid_t child;
     int action;
 
-    if (strchr(args[0], '/') != NULL)
+    if (access(args[0], X_OK) == 0)
     {
         child = fork();
         if (child == 0)
@@ -24,7 +24,36 @@ int execute(char **args, char **argv)
         return WEXITSTATUS(action);
     }
 
-    fprintf(stderr, "%s: command not found\n", args[0]);
-    return -1;
-}
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path); 
+    char *token = strtok(path_copy, ":");
+    while (token != NULL)
+    {
+        char command_path[1024]; 
+        snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
+        
+        if (access(command_path, X_OK) == 0)
+        {
+            child = fork();
+            if (child == 0)
+            {
+                if (execve(command_path, args, envir) == -1)
+                {
+                    perror(argv[0]); 
+                    _exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                waitpid(child, &action, 0);
+                freeArr(args);
+            }
+            free(path_copy);
+            return WEXITSTATUS(action);
+        }
+        token = strtok(NULL, ":");
+    }
 
+    fprintf(stderr, "%s: command not found\n", args[0]);
+    free(path_copy);
+    return -1; 
