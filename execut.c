c49669ccd9732072e956a3d87017d5d2a7802d20
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-20 00:13:59 +0200

    execute a command with the full path

--- execut.c
+++ execut.c
@@ -1,63 +1,30 @@
 #include "simple_shell.h"
 
-/* int execute(char **args, char **argv) 
-{
-    pid_t child;
-    int action;
-
-    child = fork();
-    if (child == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror(argv[0]);
-            _exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        waitpid(child, &action, 0);
-        freeArr(args);
-    }
-    return WEXITSTATUS(action);
-} */
-
 int execute(char **args, char **argv) 
 {
     pid_t child;
     int action;
 
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *token = strtok(path_copy, ":");
-    while (token != NULL)
+    if (strchr(args[0], '/') != NULL)
     {
-        char command_path[1024];  
-        snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
-        
-        if (access(command_path, X_OK) == 0) 
+        child = fork();
+        if (child == 0)
         {
-            child = fork();
-            if (child == 0)
+            if (execve(args[0], args, envir) == -1)
             {
-                if (execve(command_path, args, envir) == -1)
-                {
-                    perror(argv[0]); 
-                    _exit(EXIT_FAILURE);
-                }
+                perror(argv[0]); 
+                _exit(EXIT_FAILURE);
             }
-            else
-            {
-                waitpid(child, &action, 0);
-                freeArr(args);
-            }
-            free(path_copy);
-            return WEXITSTATUS(action);
         }
-        token = strtok(NULL, ":");
+        else
+        {
+            waitpid(child, &action, 0);
+            freeArr(args);
+        }
+        return WEXITSTATUS(action);
     }
 
     fprintf(stderr, "%s: command not found\n", args[0]);
-    free(path_copy);
     return -1;
 }
+
