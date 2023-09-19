17c6cd77653b8f3bea14fce633c3dc2779b4db57
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-20 00:33:08 +0200

    

--- execut.c
+++ execut.c
@@ -4,59 +4,21 @@ int execute(char **args, char **argv)
 {
     pid_t child;
     int action;
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path); 
-    char *token = strtok(path_copy, ":");
-    char command_path[1024];
 
-
-    if (strchr(args[0], '/') != NULL)
+    child = fork();
+    if (child == 0)
     {
-        child = fork();
-        if (child == 0)
+        if (execvp(args[0], args) == -1)
         {
-            if (execve(args[0], args, envir) == -1)
-            {
-                perror(argv[0]); 
-                _exit(EXIT_FAILURE);
-            }
+            perror(argv[0]);
+            _exit(EXIT_FAILURE);
         }
-        else
-        {
-            waitpid(child, &action, 0);
-            freeArr(args);
-        }
-        return WEXITSTATUS(action);
     }
-
-    
-    while (token != NULL)
+    else
     {
-        snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
-        
-        if (access(command_path, X_OK) == 0)
-        {
-            child = fork();
-            if (child == 0)
-            {
-                if (execve(command_path, args, envir) == -1)
-                {
-                    perror(argv[0]); 
-                    _exit(EXIT_FAILURE);
-                }
-            }
-            else
-            {
-                waitpid(child, &action, 0);
-                freeArr(args);
-            }
-            free(path_copy);
-            return WEXITSTATUS(action);
-        }
-        token = strtok(NULL, ":");
+        waitpid(child, &action, 0);
+        freeArr(args);
     }
-
-    fprintf(stderr, "%s: command not found\n", args[0]);
-    free(path_copy);
-    return -1;
+    return WEXITSTATUS(action);
 }
+
