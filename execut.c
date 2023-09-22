b9ecc54690e52a9edc35983665a4da69345556c3
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2023-09-23 00:55:11 +0200

    updat execut func

--- execut.c
+++ execut.c
@@ -1,83 +1,107 @@
 #include "simple_shell.h"
 
 /**
- * execute - a function that execute a string
- * @args: the parameter
+ * execute - Execute a command
+ * @args: An array of command arguments
  *
- * Return: an int
+ * Return: Exit status of the executed command
  */
 
 int execute(char **args)
+
 {
-	pid_t child;
-	int action;
-	char command_path[1024];
-	char *path;
-	char *path_copy;
-	char *token;
-	int found = 0;
+    if (_strcmp(args[0], "exit") == 0)
+    {
+        exit(0);
+    }
 
-	if (_strcmp(args[0], "exit") == 0)
-	{
-		exit(0);
-	}
+    if (strchr(args[0], '/') != NULL)
+    {
+        return executeCommand(args);
+    }
+    else
+    {
+        return executePathCommand(args);
+    }
+}
 
-	if (strchr(args[0], '/') != NULL)
-	{
-		child = fork();
-		if (child == 0)
-		{
-			if (execvp(args[0], args) == -1)
-			{
-				perror(args[0]);
-				_exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(child, &action, 0);
-			_freeArr(args);
-		}
-		return (WEXITSTATUS(action));
-	}
+/**
+ * executeCommand - Execute a command with a full path
+ * @args: An array of command arguments
+ *
+ * Return: Exit status of the executed command
+ */
+int executeCommand(char **args)
+{
+    pid_t child;
+    int action;
 
-	path = getenv("PATH");
-	path_copy = _strdup(path);
-	token = strtok(path_copy, ":");
+    child = fork();
+    if (child == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror(args[0]);
+            _exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        waitpid(child, &action, 0);
+        _freeArr(args);
+    }
 
-	while (token != NULL)
-	{
-		snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
+    return WEXITSTATUS(action);
+}
 
-		if (access(command_path, X_OK) == 0)
-		{
-			found = 1;
-			break;
-		}
-		token = strtok(NULL, ":");
-	}
+/**
+ * executePathCommand - Execute a command using PATH
+ * @args: An array of command arguments
+ *
+ * Return: Exit status of the executed command
+ */
+int executePathCommand(char **args)
+{
+    char command_path[1024];
+    char *path = getenv("PATH");
+    char *path_copy = _strdup(path);
+    char *token = strtok(path_copy, ":");
+    int found = 0;
+    int action;
 
-	if (found)
-	{
-		child = fork();
-		if (child == 0)
-		{
-			if (execvp(command_path, args) == -1)
-			{
-				perror(args[0]);
-				_exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(child, &action, 0);
-			_freeArr(args);
-		}
-		free(path_copy);
-		return (WEXITSTATUS(action));
-	}
+    while (token != NULL)
+    {
+        snprintf(command_path, sizeof(command_path), "%s/%s", token, args[0]);
 
-	fprintf(stderr, "%s: command not found\n", args[0]);
-	free(path_copy);
-	return (-1);
+        if (access(command_path, X_OK) == 0)
+        {
+            found = 1;
+            break;
+        }
+        token = strtok(NULL, ":");
+    }
+
+    if (found)
+    {
+        pid_t child = fork();
+        if (child == 0)
+        {
+            if (execvp(command_path, args) == -1)
+            {
+                perror(args[0]);
+                _exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            waitpid(child, &action, 0);
+            _freeArr(args);
+        }
+        free(path_copy);
+        return WEXITSTATUS(action);
+    }
+
+    fprintf(stderr, "%s: command not found\n", args[0]);
+    free(path_copy);
+    return -1;
 }
